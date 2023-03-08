2329d2b735c4ca7f8fcd7d715d2efe0402218ceb
lmnghia911 <lmnghia911@gmail.com>
2023-03-08 21:14:27 +0700

    connect GraphQl sever with Mongo database

--- app.js
+++ app.js
@@ -1,13 +1,35 @@
 const express = require('express')
 const { ApolloServer } = require('apollo-server-express')
+const mongoose = require('mongoose')
 
 //Load schema & resolvers
 const typeDefs = require('./schema/schema')
 const resolvers = require('./resolver/resolver')
 
+//Load db methods
+const mongoDataMethods = require('./data/db')
+
+//Connect to MongoDB
+const connectDB = async () => {
+    try {
+        await mongoose.connect('mongodb+srv://leminhnghia:1234@tutorialgraphql.airkevq.mongodb.net/?retryWrites=true&w=majority', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        })
+
+        console.log('MongoDB Connected')
+    } catch (err) {
+        console.log(err.message)
+        process.exit(1)
+    }
+}
+
+connectDB()
+
 const server = new ApolloServer({
     typeDefs,
-    resolvers
+    resolvers,
+    context: () => ({ mongoDataMethods })
 })
 
 const app = express()
--- resolver/resolver.js
+++ resolver/resolver.js
@@ -1,23 +1,22 @@
-const { books, authors } = require('../data/static')
 const resolvers = {
     //QUERY
     Query: {
-        books: () => books,
-        book: (parent, args) => books.find(book => book.id == args.id),
-        authors: () => authors,
-        author: (parent, args) => authors.find(author => author.id == args.id)
+        books: async (parent, args, context) => await context.mongoDataMethods.getAllBooks(), //Cach khac cho "context" o dong 5
+        book: async (parent, args, { mongoDataMethods }) => await mongoDataMethods.getBookById(args.id), //Cach khac cho args o dong 7
+        authors: async (parent, args, { mongoDataMethods }) => await mongoDataMethods.getAllAuthors(),
+        author: async (parent, { id }, { mongoDataMethods }) => await mongoDataMethods.getAuthorById(id)
     },
     Book: {
-        author: (parent) => authors.find(author => author.id == parent.authorId)
+        author: async ({ authorId }, args, { mongoDataMethods }) => await mongoDataMethods.getAuthorById(authorId)
     },
     Author: {
-        books: (parent) => books.filter(book => book.authorId == parent.id)
+        books: async ({ id }, args, { mongoDataMethods }) => await mongoDataMethods.getAllBooks({ authorId: id })
     },
 
     //MUTATION
     Mutation: {
-        createAuthor: (parent, args) => args,
-        createBook: (parent, args) => args
+        createAuthor: async (parent, args, { mongoDataMethods }) => await mongoDataMethods.createAuthor(parent, args),
+        createBook: async (parent, args, { mongoDataMethods }) => await mongoDataMethods.createBook(parent, args)
     }
 }
 
